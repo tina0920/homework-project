d2c2c10ae13b6cd3f9ea67fb92f1d31569156362
iMac_001 <cwa000001@iMac-001deiMac.local>
2022-12-12 11:11:57 +0800

    fix issue

--- src/auto-imports.d.ts
+++ src/auto-imports.d.ts
@@ -38,7 +38,7 @@ declare global {
   const h: typeof import('vue')['h']
   const ignorableWatch: typeof import('@vueuse/core')['ignorableWatch']
   const inject: typeof import('vue')['inject']
-  const isDark: typeof import('./composables/dark.js')['isDark']
+  const isDark: typeof import('./composables/dark')['isDark']
   const isDefined: typeof import('@vueuse/core')['isDefined']
   const isProxy: typeof import('vue')['isProxy']
   const isReactive: typeof import('vue')['isReactive']
@@ -67,7 +67,7 @@ declare global {
   const onUnmounted: typeof import('vue')['onUnmounted']
   const onUpdated: typeof import('vue')['onUpdated']
   const pausableWatch: typeof import('@vueuse/core')['pausableWatch']
-  const preferredDark: typeof import('./composables/dark.js')['preferredDark']
+  const preferredDark: typeof import('./composables/dark')['preferredDark']
   const provide: typeof import('vue')['provide']
   const reactify: typeof import('@vueuse/core')['reactify']
   const reactifyObject: typeof import('@vueuse/core')['reactifyObject']
@@ -98,7 +98,7 @@ declare global {
   const toReactive: typeof import('@vueuse/core')['toReactive']
   const toRef: typeof import('vue')['toRef']
   const toRefs: typeof import('vue')['toRefs']
-  const toggleDark: typeof import('./composables/dark.js')['toggleDark']
+  const toggleDark: typeof import('./composables/dark')['toggleDark']
   const triggerRef: typeof import('vue')['triggerRef']
   const tryOnBeforeMount: typeof import('@vueuse/core')['tryOnBeforeMount']
   const tryOnBeforeUnmount: typeof import('@vueuse/core')['tryOnBeforeUnmount']
@@ -249,7 +249,7 @@ declare global {
   const useTransition: typeof import('@vueuse/core')['useTransition']
   const useUrlSearchParams: typeof import('@vueuse/core')['useUrlSearchParams']
   const useUserMedia: typeof import('@vueuse/core')['useUserMedia']
-  const useUserStore: typeof import('./store/user.js')['useUserStore']
+  const useUserStore: typeof import('./store/user')['useUserStore']
   const useVModel: typeof import('@vueuse/core')['useVModel']
   const useVModels: typeof import('@vueuse/core')['useVModels']
   const useVibrate: typeof import('@vueuse/core')['useVibrate']
@@ -318,7 +318,7 @@ declare module 'vue' {
     readonly h: UnwrapRef<typeof import('vue')['h']>
     readonly ignorableWatch: UnwrapRef<typeof import('@vueuse/core')['ignorableWatch']>
     readonly inject: UnwrapRef<typeof import('vue')['inject']>
-    readonly isDark: UnwrapRef<typeof import('./composables/dark.js')['isDark']>
+    readonly isDark: UnwrapRef<typeof import('./composables/dark')['isDark']>
     readonly isDefined: UnwrapRef<typeof import('@vueuse/core')['isDefined']>
     readonly isProxy: UnwrapRef<typeof import('vue')['isProxy']>
     readonly isReactive: UnwrapRef<typeof import('vue')['isReactive']>
@@ -347,7 +347,7 @@ declare module 'vue' {
     readonly onUnmounted: UnwrapRef<typeof import('vue')['onUnmounted']>
     readonly onUpdated: UnwrapRef<typeof import('vue')['onUpdated']>
     readonly pausableWatch: UnwrapRef<typeof import('@vueuse/core')['pausableWatch']>
-    readonly preferredDark: UnwrapRef<typeof import('./composables/dark.js')['preferredDark']>
+    readonly preferredDark: UnwrapRef<typeof import('./composables/dark')['preferredDark']>
     readonly provide: UnwrapRef<typeof import('vue')['provide']>
     readonly reactify: UnwrapRef<typeof import('@vueuse/core')['reactify']>
     readonly reactifyObject: UnwrapRef<typeof import('@vueuse/core')['reactifyObject']>
@@ -378,7 +378,7 @@ declare module 'vue' {
     readonly toReactive: UnwrapRef<typeof import('@vueuse/core')['toReactive']>
     readonly toRef: UnwrapRef<typeof import('vue')['toRef']>
     readonly toRefs: UnwrapRef<typeof import('vue')['toRefs']>
-    readonly toggleDark: UnwrapRef<typeof import('./composables/dark.js')['toggleDark']>
+    readonly toggleDark: UnwrapRef<typeof import('./composables/dark')['toggleDark']>
     readonly triggerRef: UnwrapRef<typeof import('vue')['triggerRef']>
     readonly tryOnBeforeMount: UnwrapRef<typeof import('@vueuse/core')['tryOnBeforeMount']>
     readonly tryOnBeforeUnmount: UnwrapRef<typeof import('@vueuse/core')['tryOnBeforeUnmount']>
@@ -529,7 +529,7 @@ declare module 'vue' {
     readonly useTransition: UnwrapRef<typeof import('@vueuse/core')['useTransition']>
     readonly useUrlSearchParams: UnwrapRef<typeof import('@vueuse/core')['useUrlSearchParams']>
     readonly useUserMedia: UnwrapRef<typeof import('@vueuse/core')['useUserMedia']>
-    readonly useUserStore: UnwrapRef<typeof import('./store/user.js')['useUserStore']>
+    readonly useUserStore: UnwrapRef<typeof import('./store/user')['useUserStore']>
     readonly useVModel: UnwrapRef<typeof import('@vueuse/core')['useVModel']>
     readonly useVModels: UnwrapRef<typeof import('@vueuse/core')['useVModels']>
     readonly useVibrate: UnwrapRef<typeof import('@vueuse/core')['useVibrate']>
--- src/pages/slid.vue
+++ src/pages/slid.vue
@@ -1,119 +1,108 @@
 <script setup lang="ts">
 import gsap from 'gsap'
 // 實際清單
-const voiceData = reactive({
-  lists: [
-    {
-      pic1: 'voice-m40-50.png',
-      pic2: 'voice-balloon-nomeru.svg',
-      name: 'W.Yさま',
-      status: '50代 / 男性',
-      content: '朝、水に溶かして一袋づつ飲んでいます！水にさっと溶けてくれるので飲みやすく、どことなく甘みを感じる味です。整腸作用が効いているのか毎日快便になってお腹もスッキリした気がします！続けてみようと思います。',
-    }, {
-      pic1: 'voice-w40-50.png',
-      pic2: 'voice-balloon-haireru.svg',
-      name: 'Y.Mさま',
-      status: '40代 / 女性',
-      content: '小分けのスプレーボトルにお水で溶かして持ち歩いています！泉質は、“硫酸塩泉”。「美人の湯」とも呼ばれるくらい美肌効果もあるそうで、皮膚にも優しく、保温効果やニキビなどの効果も！？温泉を持ち歩けるなんてとても豪華ですね。',
-    }, {
-      pic1: 'voice-w60.png',
-      pic2: 'voice-balloon-tabereru.svg',
-      name: 'S.Mさま',
-      status: '60代 / 女性',
-      content: 'ご飯を炊く際に、温泉パウダーを入れてみました。パウダーを入れたご飯、入れてないご飯を食べ比べてみると、パウダーを入れた方がお米の粒が際立っていて、また甘みも感じられ、ご飯が美味しかったです。',
-    }, {
-      pic1: 'voice-m40-50.png',
-      pic2: 'voice-balloon-tabereru.svg',
-      name: 'S.Eさま',
-      status: '40代 / 男性',
-      content: '米を研いで温泉パウダーを混ぜました。だまなどにはならずすっと水に溶けてくれて満遍なく成分が行き渡った印象。お米一粒一粒が立つというのだろうか？咀嚼のたびに米が程よくほぐれいつもよりもお米そのものの旨味を感じた。',
-    }, {
-      pic1: 'voice-m20-30.png',
-      pic2: 'voice-balloon-nomeru.svg',
-      name: 'ねこぽんさま',
-      status: '20代 / 男性',
-      content: '飲んだ感じは無味無臭で、どんな飲料・食べ物にも合いそうです。パッケージが可愛いので、プレゼントにも良いかも♪現地に足を運ばなくても自宅で、食べて・飲んで・入って楽しめるのはいいですね。まだ飲みはじめたばかりですが、送料の関係もあるので定期購入でお得に買えたらいいのにな〜って思いました。',
-    }, {
-      pic1: 'voice-w20-30.png',
-      pic2: 'voice-balloon-nomeru.svg',
-      name: 'O.Sさま',
-      status: '20代 / 女性',
-      content: '粉を一口舐めてみると、わたあめみたいにほんのりと甘くて美味しかったです。粉は、口で中でサラッと溶けていきました。白湯に溶かして飲んでも美味しく飲むことができました。',
-    },
-  ],
-})
-
+const voiceDataList = reactive([
+  {
+    pic1: 'voice-m40-50.png',
+    pic2: 'voice-balloon-nomeru.svg',
+    name: 'W.Yさま',
+    status: '50代 / 男性',
+    content: '朝、水に溶かして一袋づつ飲んでいます！水にさっと溶けてくれるので飲みやすく、どことなく甘みを感じる味です。整腸作用が効いているのか毎日快便になってお腹もスッキリした気がします！続けてみようと思います。',
+  }, {
+    pic1: 'voice-w40-50.png',
+    pic2: 'voice-balloon-haireru.svg',
+    name: 'Y.Mさま',
+    status: '40代 / 女性',
+    content: '小分けのスプレーボトルにお水で溶かして持ち歩いています！泉質は、“硫酸塩泉”。「美人の湯」とも呼ばれるくらい美肌効果もあるそうで、皮膚にも優しく、保温効果やニキビなどの効果も！？温泉を持ち歩けるなんてとても豪華ですね。',
+  }, {
+    pic1: 'voice-w60.png',
+    pic2: 'voice-balloon-tabereru.svg',
+    name: 'S.Mさま',
+    status: '60代 / 女性',
+    content: 'ご飯を炊く際に、温泉パウダーを入れてみました。パウダーを入れたご飯、入れてないご飯を食べ比べてみると、パウダーを入れた方がお米の粒が際立っていて、また甘みも感じられ、ご飯が美味しかったです。',
+  }, {
+    pic1: 'voice-m40-50.png',
+    pic2: 'voice-balloon-tabereru.svg',
+    name: 'S.Eさま',
+    status: '40代 / 男性',
+    content: '米を研いで温泉パウダーを混ぜました。だまなどにはならずすっと水に溶けてくれて満遍なく成分が行き渡った印象。お米一粒一粒が立つというのだろうか？咀嚼のたびに米が程よくほぐれいつもよりもお米そのものの旨味を感じた。',
+  }, {
+    pic1: 'voice-m20-30.png',
+    pic2: 'voice-balloon-nomeru.svg',
+    name: 'ねこぽんさま',
+    status: '20代 / 男性',
+    content: '飲んだ感じは無味無臭で、どんな飲料・食べ物にも合いそうです。パッケージが可愛いので、プレゼントにも良いかも♪現地に足を運ばなくても自宅で、食べて・飲んで・入って楽しめるのはいいですね。まだ飲みはじめたばかりですが、送料の関係もあるので定期購入でお得に買えたらいいのにな〜って思いました。',
+  }, {
+    pic1: 'voice-w20-30.png',
+    pic2: 'voice-balloon-nomeru.svg',
+    name: 'O.Sさま',
+    status: '20代 / 女性',
+    content: '粉を一口舐めてみると、わたあめみたいにほんのりと甘くて美味しかったです。粉は、口で中でサラッと溶けていきました。白湯に溶かして飲んでも美味しく飲むことができました。',
+  },
+])
 // Dom 渲染的清單 ＝ 實際清單*3
-const voiceDataList = computed(() => {
-  return voiceData.lists.concat(voiceData.lists, voiceData.lists)
+const processList = computed(() => {
+  return voiceDataList.concat(voiceDataList, voiceDataList)
 })
 
-// 進度條 相關
-const progressbar = ref(null)
-const progressbarWidth = 1 / voiceData.lists.length * 100
-const progressbarX = ref(0)
+const currentItem = computed(() => voiceDataList.length) // 當前項目
+const previousItem = ref(0) // 前一個項目
+const moveX = computed(() => currentItem.value * -100)
 
-const currentItem = ref(0) // 當前項目
-const lastItem = ref(0) // 前一個項目
-const moveX = ref(0)
+const progressbarX = ref(0) // 進度條大小
+
+//* 切換當前項目
+function changeVoiceItem(btn: number) {
+  previousItem.value = currentItem.value
+  currentItem.value += btn
+  
+  // 進度條
+  const progressbarCurrent = currentItem.value % voiceDataList.length
+  progressbarX.value = progressbarCurrent >= 0 ? progressbarCurrent + 1 : progressbarCurrent + 7
+  // 清單位移
+  moveX.value = currentItem.value * -100
+  
+  moveVoiceListLis()
+}
+
+//* 清單 位移
 const timeline = gsap.timeline()
+function moveVoiceListLis() {
+  timeline.to('.progressbar', { direction: 1, scaleX: progressbarX.value, transformOrigin: '0%' })
+    .to(`.item${currentItem.value}`, { direction: 0.3, opacity: 1 }, '<')
+    .to(`.item${previousItem.value}`, { direction: 0.3, opacity: 0.5 }, '<')
+    .to('.list-item', { direction: 0.5, xPercent: moveX.value }, '<')
+
+  // 如果是第一輪最後一個||第三輪第一個 => 彈回第二輪的第一個||最後一個
+  if (currentItem.value === (voiceDataList.length - 1) || currentItem.value === (voiceDataList.length * 2))
+    backSecondList()
+}
 
 // 回到第二輪的 第一個||最後一個
 function backSecondList() {
   // 計算 第二輪的第一個||最後一個
-  const secondItem = (currentItem.value % voiceData.lists.length) + voiceData.lists.length
 
-  lastItem.value = currentItem.value
-  currentItem.value = secondItem
+  previousItem.value = currentItem.value
+  currentItem.value = (currentItem.value % voiceDataList.length) + voiceDataList.length
   moveX.value = currentItem.value * -100
 
   // 回第二輪的第一個||最後一個
   timeline.set('.voiceList', { direction: 0, xPercent: moveX.value })
     .set(`.item${currentItem.value}`, { direction: 0, opacity: 1 }, '<')
-    .set(`.item${lastItem.value}`, { direction: 0, opacity: 0.5 }, '<')
-}
-
-// 清單 位移
-function moveVoiceListLis() {
-  timeline.to('.progressbar', { direction: 1, scaleX: progressbarX.value, transformOrigin: '0%' })
-    .to(`.item${currentItem.value}`, { direction: 0.3, opacity: 1 }, '<')
-    .to(`.item${lastItem.value}`, { direction: 0.3, opacity: 0.5 }, '<')
-    .to('.voiceList', { direction: 0.5, xPercent: moveX.value }, '<')
-
-  // 如果是第一輪最後一個||第三輪第一個 => 彈回第二輪的第一個||最後一個
-  if (currentItem.value === (voiceData.lists.length - 1) || currentItem.value === (voiceData.lists.length * 2))
-    backSecondList()
-}
-// 切換當前項目
-function changeVoiceItem(btn: number) {
-  lastItem.value = currentItem.value
-  currentItem.value += btn
-
-  // 進度條
-  const progressbarCurrent = currentItem.value % voiceData.lists.length
-  progressbarX.value = progressbarCurrent >= 0 ? progressbarCurrent + 1 : progressbarCurrent + 7
-  // 清單位移
-  moveX.value = currentItem.value * -100
-
-  moveVoiceListLis()
+    .set(`.item${previousItem.value}`, { direction: 0, opacity: 0.5 }, '<')
 }
 
 onMounted(() => {
-  // 預設在第二輪第一個
-  currentItem.value = voiceData.lists.length
-  moveX.value = currentItem.value * -100
 
-  timeline.to('.voiceList', { direction: 0.5, xPercent: moveX.value })
-    .to(`.item${currentItem.value}`, { direction: 1, opacity: 1 }, '<')
+  timeline.set('.voiceList', { direction: 0.5, xPercent: moveX.value })
+    .set(`.item${currentItem.value}`, { direction: 1, opacity: 1 }, '<')
 
-  // 進度條 預設 寬度
-  progressbar.value.style.width = `${progressbarWidth}%`
 })
 </script>
 
 <template>
-  <div class="slidPage">
-    <div class="left">
+  <div class="slid-page">
+    <div class="left-area">
       <div class="title">
         <h6>Voice</h6>
         <h3 mt>
@@ -121,10 +110,10 @@ onMounted(() => {
         </h3>
       </div>
     </div>
-    <div class="right">
-      <div class="voicListBox">
-        <div class="voiceLists">
-          <div v-for="(item, index) in voiceDataList" :key="item" class="voiceList " :class="`item${index}`">
+    <div class="right-area">
+      <div class="list-box">
+        <div class="process-list">
+          <div v-for="(item, index) in processList" :key="item.name" class="list-item " :class="`item${index}`">
             <img class="pic1" :src="`src/assets/images/${item.pic1}`" alt="">
             <img class="pic2" :src="`src/assets/images/${item.pic2}`" alt="">
             <h6 m-t-20px>
@@ -139,20 +128,20 @@ onMounted(() => {
           </div>
         </div>
       </div>
-      <a class="arrowIcon" left--23px @click="changeVoiceItem(-1)"><div i-ion:chevron-back-outline c-white text-2xl /></a>
-      <a class="arrowIcon" left-357px @click="changeVoiceItem(1)"><div i-ion:chevron-forward c-white text-2xl /></a>
-      <div class="swiperPagination">
-        <span ref="progressbar" class="progressbar" />
+      <a class="arrow-icon" left--23px @click="changeVoiceItem(-1)"><div i-ion:chevron-back-outline c-white text-2xl /></a>
+      <a class="arrow-icon" left-357px @click="changeVoiceItem(1)"><div i-ion:chevron-forward c-white text-2xl /></a>
+      <div class="swiper-pagination">
+        <span :style="{ width: 1 / voiceDataList.length * 100 + '%' }" class="progressbar" />
       </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.slidPage{
+.slid-page{
   @apply pt-10 w-screen h-screen flex c-black truncate;
   background-image: url("../assets/images/bg.jpg");
-  .left{
+  .left-area{
     @apply relative w-20% h-100%;
 
     &::before{
@@ -171,17 +160,17 @@ onMounted(() => {
     }
 
   }
-  .right {
+  .right-area {
     @apply relative w-80% h-100% flex flex-col justify-center;
-    .voicListBox {
+    .list-box {
       @apply py-80px truncate;
-      .voiceLists{
+      .process-list{
         @apply relative whitespace-nowrap;
 
         &::before {
           @apply absolute left-0  top-0 w-380px h-100% border-8px border-black content-empty ;
         }
-        .voiceList {
+        .list-item {
           @apply relative w-380px max-w-380px opacity-50 inline-block py-50px px-45px v-start ;
           .pic1{
             @apply w-50% translate-x-50%;
@@ -196,7 +185,7 @@ onMounted(() => {
         }
       }
     }
-    .arrowIcon {
+    .arrow-icon {
       @apply absolute top-50% translate-y--50% w-46px h-46px  text-center bg-black rounded-full cursor-pointer flex items-center justify-center;
       transition: background-color .25s ease 0s,border-color .25s ease 0s;
       &:hover {
@@ -204,7 +193,7 @@ onMounted(() => {
       }
 
     }
-    .swiperPagination {
+    .swiper-pagination {
       @apply w-100% h-4px bg-black;
       .progressbar {
         @apply h-100% bg-#f72f8d block;
